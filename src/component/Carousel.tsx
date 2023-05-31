d1002178986c3cc60e24e2ce7c951d0e00a21456
Ashish Gupta <ashishgupt25@gmail.com>
2023-05-31 18:20:03 +0530

    feat(Carousel): left and right arrow click to change image

--- src/component/Carousel.tsx
+++ src/component/Carousel.tsx
@@ -1,6 +1,10 @@
 import { useEffect, useState } from "react";
 import { ImageData } from "../App";
 import "./carousel.css";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
+import PlayCircleIcon from "@mui/icons-material/PlayCircle";
+import PauseCircleIcon from "@mui/icons-material/PauseCircle";
 
 type Props = {
     imageData: ImageData[];
@@ -8,14 +12,35 @@ type Props = {
 
 const Carousel = ({ imageData }: Props) => {
     const [isSelected, setIsSelected] = useState<ImageData>(imageData[0]);
+    const [selectedIndex, setSelectedIndex] = useState(0);
+    const [isPlaying, setIsPlaying] = useState(false);
 
-    const handleClick = (image: ImageData) => {
-        setIsSelected(image);
+    const handleClick = (updatedIndex: number) => {
+        setIsSelected(imageData[updatedIndex]);
+        setSelectedIndex(updatedIndex);
     };
 
     useEffect(() => {
         setIsSelected(imageData[0]);
     }, [imageData]);
+
+    const handleLeftImageChange = () => {
+        let updatedIndex = selectedIndex - 1;
+        if (updatedIndex < 0) {
+            updatedIndex = imageData.length - 1;
+        }
+        handleClick(updatedIndex);
+    };
+
+    const handleRightImageChange = () => {
+        let updatedIndex = selectedIndex + 1;
+        if (updatedIndex >= imageData.length) {
+            updatedIndex = 0;
+        }
+        handleClick(updatedIndex);
+    };
+
+    console.log(selectedIndex);
 
     return (
         <div className="main-container">
@@ -23,8 +48,11 @@ const Carousel = ({ imageData }: Props) => {
                 <img src={isSelected?.urls.regular} className="top-image" />
             </div>
             <div className="bottom-container">
-                {imageData.map((image) => (
-                    <div onClick={() => handleClick(image)} key={image.id}>
+                <div className="arrow-back" onClick={handleLeftImageChange}>
+                    <ArrowBackIosIcon />
+                </div>
+                {imageData.map((image, index) => (
+                    <div onClick={() => handleClick(index)} key={image.id}>
                         <img
                             src={image.urls.regular}
                             alt={`Image ${image.id}`}
@@ -32,6 +60,12 @@ const Carousel = ({ imageData }: Props) => {
                         />
                     </div>
                 ))}
+                <div className="arrow-right" onClick={handleRightImageChange}>
+                    <ArrowForwardIosIcon />
+                </div>
+                <div>
+                    <button></button>
+                </div>
             </div>
         </div>
     );
